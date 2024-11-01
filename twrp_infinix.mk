0eb27d3a526fb9941b0ab71a26c9e767303b70fd
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2024-11-01 18:37:06 +0500

    Update twrp_infinix.mk

--- twrp_infinix.mk
+++ twrp_infinix.mk
@@ -8,7 +8,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
