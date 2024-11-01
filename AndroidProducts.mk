0c7e984a2e200121dc5e3bc5a10972b8f37e76e9
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2024-11-01 18:51:11 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_infinix-x6812.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_infinix-x6812-user \
-    twrp_infinix-x6812-userdebug \
-    twrp_infinix-x6812-eng
+    twrp_Infinix-X6812-user \
+    twrp_Infinix-X6812-userdebug \
+    twrp_Infinix-X6812-eng
