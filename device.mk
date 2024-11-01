7c833f19cc180f955be8cc27c4563bdf78eb9d22
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2024-11-01 19:26:51 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/x6812
+LOCAL_PATH := device/infinix/infinix-x6812
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
