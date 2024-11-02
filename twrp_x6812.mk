4283778df59faaa6110645ad65a8a0008db6961f
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2024-11-02 10:07:40 +0500

    Update and rename twrp_infinix-x6812.mk to twrp_x6812.mk

--- twrp_x6812.mk
+++ twrp_x6812.mk
@@ -16,13 +16,14 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from x6812 device
 $(call inherit-product, device/infinix/infinix-x6812/device.mk)
 
-PRODUCT_DEVICE := infinix-x6812
-PRODUCT_NAME := infinix-x6812
-PRODUCT_BRAND := infinix
-PRODUCT_MODEL := infinix x6812
-PRODUCT_MANUFACTURER := infinix
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_x6812_h6912-user 11 RP1A.200720.011 435922 release-keys"
 
 BUILD_FINGERPRINT := Infinix/X6812-GL/Infinix-X6812:11/RP1A.200720.011/230921V645:user/release-keys
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X6812
+PRODUCT_NAME := twrp_X6812
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix HOT 11s
+PRODUCT_MANUFACTURER := infinix
