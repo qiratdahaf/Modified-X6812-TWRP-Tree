cdb28c9f570687292fd7f646e0d031394a38de9c
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2024-11-25 00:49:08 +0500

    Update device.mk
    removed some incompatible code

--- device.mk
+++ device.mk
@@ -31,9 +31,46 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor \
     vendor 
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+    libhealthd.$(PRODUCT_PLATFORM)
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl
+update_engine_client
 
 # Update Engine & Update Verifier 
 PRODUCT_PACKAGES += \
@@ -43,59 +80,25 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
-#update_engine_client
+Set vendor patch level
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bootimage.build.date.utc= Thu Sep 21 20:06:22 CST 2023
+    ro.build.date.utc= 1695297982
+    ro.vendor.build.security_patch= 2023-08-05
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6768 \
     bootctrl.mt6768.recovery
 
+# Gatekeeper
 PRODUCT_PACKAGES += \
-    create_pl_dev \
-    create_pl_dev.recovery
-
-# Health HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-    libhealthd.$(PRODUCT_PLATFORM)
-
-# Soong
-#PRODUCT_SOONG_NAMESPACES += \
- #   $(LOCAL_PATH)
-
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.gatekeeper@1.0-impl
 
 # MTK Plpath Utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils.recovery
-
-# API
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
@@ -107,8 +110,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-    
-    
+
 # OEM otacerts
 PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(LOCAL_PATH)/security/infinix
+    $(LOCAL_PATH)/security/
