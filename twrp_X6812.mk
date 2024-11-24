00a841096a69d935bcb12c598856e63dfbcd91fb
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2024-11-24 23:37:45 +0500

    Update and rename twrp_x6812.mk to twrp_X6812.mk
    Priv build = stock bootimg build fingerprint
    
    build finger print = build finger print from build prop

--- twrp_X6812.mk
+++ twrp_X6812.mk
@@ -6,22 +6,24 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from x6812 device
-$(call inherit-product, device/infinix/x6812/device.mk)
+$(call inherit-product, device/infinix/X6812/device.mk)
 
 # Forcefully add mtp support (adb is already there)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-  PRIVATE_BUILD_DESC="vnd_x6812_h6912-user 11 RP1A.200720.011 435922 release-keys"
+  PRIVATE_BUILD_DESC= Infinix/X6812-GL/Infinix-X6812:11/RP1A.200720.011/230921V645:user/release-keys
   persist.sys.usb.config=mtp
 
-BUILD_FINGERPRINT := Infinix/X6812-GL/Infinix-X6812:11/RP1A.200720.011/230921V645:user/release-keys
+BUILD_FINGERPRINT := Infinix/X6812-GL/Infinix-X6812:11/RP1A.200720.011/230921V810:user/release-keys
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X6812
